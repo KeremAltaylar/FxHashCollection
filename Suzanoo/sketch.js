450866d0e7ecec0ebbb1c5ebc7ba544804123fc3
~\M/~ <61120723+KeremAltaylar@users.noreply.github.com>
2023-07-21 14:42:59 +0300

    Add files via upload

---
+++ Suzanoo/sketch.js
@@ -0,0 +1,209 @@
+var inc = fxrandRange(0.1, 50, 0.1);
+var scl = fxrandRange(20, 250, 1);
+var magv = fxrandRange(10, 20, 0.1);
+var cols, rows;
+var fr;
+var zoff = 0;
+var particles = [];
+var particles2 = [];
+var particles3 = [];
+var particles4 = [];
+var flowfield;
+var magv;
+var cr = fxrandRange(200, 255, 1);
+var cg = fxrandRange(100, 110, 1);
+var cb = fxrandRange(200, 250, 1);
+var dr = fxrandRange(0, 100, 1);
+var dg = fxrandRange(150, 200, 1);
+var db = fxrandRange(10, 150, 1);
+var indexk = 0;
+var sw1 = fxrandRange(0.1, 0.5, 0.1);
+var sw2 = fxrandRange(0.1, 0.5, 0.1);
+var mes1a = fxrandRange(0.1, 4, 0.1);
+var mes2a = fxrandRange(0.1, 8, 0.1);
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  cols = floor(windowWidth / scl);
+  rows = floor(windowHeight / scl);
+  fr = createP("");
+  flowfield = new Array(cols * rows);
+  for (i = 0; i < 100; i++) {
+    particles[i] = new Particle(
+      40,
+      40,
+      60,
+      fxrand() * 100,
+      fxrand() * 100,
+      0.1
+    );
+  }
+
+  for (i = 0; i < 100; i++) {
+    particles4[i] = new Particle2(
+      60,
+      50,
+      50,
+      fxrand() * 500,
+      fxrand() * 500,
+      0.1
+    );
+  }
+  for (i = 0; i < 100; i++) {
+    particles3[i] = new Particle3(
+      40,
+      70,
+      30,
+      fxrand() * windowWidth,
+      fxrand() * windowHeight,
+      0.2
+    );
+  }
+  // background(235, 215, 141);
+  background(180);
+}
+
+function draw() {
+  if (indexk > 400) {
+    noLoop();
+  }
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * rows;
+      flowfield[index] = v;
+      var angle = noise(19, 19, 19) * yoff * fxrand();
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(magv);
+      xoff += inc;
+      // stroke(255, 130);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(0.1);
+      // line(0, 0, scl, 0);
+      // pop(); //fill(r);
+
+      //rect(scl * x, scl * y, scl, scl);
+    }
+    yoff += inc;
+    zoff += 0.8;
+  }
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+  for (var i = 0; i < particles2.length; i++) {
+    particles2[i].follow(flowfield);
+    particles2[i].update();
+    particles2[i].edges();
+    particles2[i].show();
+  }
+  for (var i = 0; i < particles3.length; i++) {
+    particles3[i].follow(flowfield);
+    particles3[i].update();
+    particles3[i].edges();
+    particles3[i].show();
+  }
+  for (var i = 0; i < particles4.length; i++) {
+    particles4[i].follow(flowfield);
+    particles4[i].update();
+    particles4[i].edges();
+    particles4[i].show();
+  }
+  push();
+  rectMode(RADIUS);
+  //fill(255, 1 * sin(millis() * 3000));
+  noStroke();
+  // rect(
+  //   windowWidth / 2,
+  //   windowHeight / 2,
+  //   windowWidth / 2 - 30,
+  //   windowHeight / 2 - 30
+  // );
+  pop();
+  indexk = indexk + 1;
+  //console.log(indexk);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  indexk = 0;
+  loop();
+  cols = floor(windowWidth / scl);
+  rows = floor(windowHeight / scl);
+  //fr = createP("");
+  flowfield = new Array(cols * rows);
+
+  for (i = 0; i < 100; i++) {
+    particles[i] = new Particle(
+      cr,
+      cg,
+      cb,
+      (fxrand() * i) / 2 + windowWidth / 2,
+      (fxrand() * i) / 3 + windowHeight / 2,
+      sw1,
+      2
+    );
+  }
+
+  for (i = 0; i < 100; i++) {
+    particles4[i] = new Particle2(
+      250,
+      25,
+      20,
+      fxrand() * i * i + windowWidth / 2,
+      fxrand() * i + windowHeight / 4,
+      0.1,
+      3
+    );
+  }
+  for (i = 0; i < 100; i++) {
+    particles3[i] = new Particle3(
+      255,
+      70,
+      70,
+      fxrand() * i * 8 + windowWidth / 4,
+      fxrand() * i * i + windowHeight / 2,
+      0.1,
+      5
+    );
+  }
+  push();
+  noStroke();
+  background(0);
+  rectMode(RADIUS);
+  fill(0);
+  //fill(alpha(50));
+  rect(
+    windowWidth / 2,
+    windowHeight / 2,
+    windowWidth / 2 - 30,
+    windowHeight / 2 - 30
+  );
+
+  rectMode(RADIUS);
+  //fill(0, 1 * sin(millis() * 1000));
+  // noStroke();
+  // rect(
+  //   windowWidth / 2,
+  //   windowHeight / 2,
+  //   windowWidth / 2 - 30,
+  //   windowHeight / 2 - 30
+  // );
+  pop();
+}
+
+function fxrandRange(min, max, step) {
+  value = Math.round((fxrand() * (max - min)) / step);
+  return value * step + min;
+}
+
+window.$fxhashFeatures = {
+  Dargh: magv,
+  XAE: inc,
+  Golbarth: scl,
+};
---
+++ XXX/sketch.js
@@ -0,0 +1,166 @@
+var inc = fxrandRange(2, 3, 0.1);
+var scl = fxrandRange(100, 200, 1);
+var magv = fxrandRange(0.11, 0.22, 0.1);
+var cols, rows;
+var fr;
+var zoff = 0;
+var particles = [];
+var particles2 = [];
+var flowfield;
+var magv;
+var cr = fxrandRange(170, 255, 1);
+//var cg = random(0, 255);
+var cg = fxrandRange(100, 250, 1);
+var cb = fxrandRange(180, 200, 1);
+var fr = fxrandRange(170, 255, 1);
+//var cg = random(0, 255);
+var fg = fxrandRange(10, 150, 1);
+var fb = fxrandRange(10, 100, 1);
+var sh = fxrandRange(10, 300, 1);
+var sw = fxrandRange(10, 300, 1);
+var conscol = 10;
+var indexk = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  cols = floor(windowWidth / scl);
+  rows = floor(windowHeight / scl);
+  fr = createP("");
+  flowfield = new Array(cols * rows);
+  for (i = 0; i < 100; i++) {
+    particles[i] = new Particle(
+      cr - i / conscol,
+      cb - i / conscol,
+      cg - i / conscol,
+      fxrandRange(0, windowWidth, 1),
+      fxrandRange(0, windowHeight, 1),
+      0.1
+    );
+  }
+  for (i = 0; i < 100; i++) {
+    particles2[i] = new Particle2(
+      fr - i / conscol,
+      fb - i / conscol,
+      fg - i / conscol,
+      (fxrand() * windowWidth) / 10,
+      (fxrand() * windowHeight) / 10,
+      0.1
+    );
+  }
+  background(fxrandRange(0, 29, 1));
+}
+
+function draw() {
+  if (indexk > 1800) {
+    noLoop();
+  }
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      flowfield[index] = v;
+      var angle = xoff * yoff;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(magv);
+      xoff += inc;
+      stroke(0, 130);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(0.1);
+      // line(0, 0, scl, 0);
+      // pop(); //fill(r);
+
+      //rect(scl * x, scl * y, scl, scl);
+    }
+    yoff += inc;
+    zoff += 0.000008;
+  }
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+  for (var i = 0; i < particles2.length; i++) {
+    particles2[i].follow(flowfield);
+    particles2[i].update();
+    particles2[i].edges();
+    particles2[i].show();
+  }
+  // push();
+  // rectMode(RADIUS);
+  // fill(255, 1 * sin(millis() * 1000));
+  // noStroke();
+  // rect(
+  //   windowWidth / 2,
+  //   windowHeight / 2,
+  //   windowWidth / 2 - 30,
+  //   windowHeight / 2 - 30
+  // );
+  // pop();
+  indexk = indexk + 1;
+  //console.log(indexk);
+  // push();
+  // fill(255);
+  // noStroke();
+  // ellipse(windowWidth / 2, windowHeight / 2, sw, sh);
+  // pop();
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+  cols = floor(windowWidth / scl);
+  rows = floor(windowHeight / scl);
+  //fr = createP("");
+  flowfield = new Array(cols * rows);
+
+  for (i = 0; i < 1000; i++) {
+    //particles[i] = new Particle(3, 0 + i * 20, 0);
+    particles[i] = new Particle(
+      cr,
+      cg,
+      cb,
+      fxrand() * windowWidth,
+      fxrand() * windowHeight
+    );
+  }
+  push();
+  noStroke();
+  background(255);
+  rectMode(RADIUS);
+  fill(255);
+  //fill(alpha(50));
+  rect(
+    windowWidth / 2,
+    windowHeight / 2,
+    windowWidth / 2 - 30,
+    windowHeight / 2 - 30
+  );
+
+  rectMode(RADIUS);
+  fill(255, 1 * sin(millis() * 1000));
+  noStroke();
+  rect(
+    windowWidth / 2,
+    windowHeight / 2,
+    windowWidth / 2 - 30,
+    windowHeight / 2 - 30
+  );
+  pop();
+}
+
+function fxrandRange(min, max, step) {
+  value = Math.round((fxrand() * (max - min)) / step);
+  return value * step + min;
+}
+
+window.$fxhashFeatures = {
+  "Red Value": cr,
+  "Green Value": cg,
+  "Flow Magnitude": magv,
+  "Blue Value": cb,
+  "Flow Increase": inc,
+  "Cell Scale": scl,
+};
